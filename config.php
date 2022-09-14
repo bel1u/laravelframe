80ceef1d252516337eeccd72140dd0c2cac26bd9
bel1u <101239725+bel1u@users.noreply.github.com>
2022-09-14 09:26:00 +0500

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+const DB_HOSTNAME = 'mysqlis.radiotech.su';
+const DB_USERNAME = 'is341_20_qwe';
+const DB_PASSWORD = 'rhjirfrfhnjirf';
+const DB_DATABASE = 'is341_20_mvc';
+const DB_PORT = '3306'; # Default 3306
